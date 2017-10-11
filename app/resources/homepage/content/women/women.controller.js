552b78a426c880253dc80b1a3a184487a469de00
Dung Le <dungle1811@gmail.com>
2017-10-11 16:39:58 +0700

    ! Paginate products in women catalog

--- app/resources/homepage/content/women/women.controller.js
+++ app/resources/homepage/content/women/women.controller.js
@@ -3,5 +3,7 @@ import { catalog } from '../../fixture';
 /* @ngInject */
 export default $scope => {
   $scope.list = catalog[1].products;
-  console.log(catalog[1].products);
+  
+  $scope.currentPage = 1;
+  $scope.pageSize = 6;
 }
