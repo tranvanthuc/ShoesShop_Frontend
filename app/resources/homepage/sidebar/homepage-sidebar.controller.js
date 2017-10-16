3975a57c44463decf263721031b18a8b288e0559
Dung Le <dungle1811@gmail.com>
2017-10-16 13:46:34 +0700

    * Fix header scrolling event

--- app/resources/homepage/sidebar/homepage-sidebar.controller.js
+++ app/resources/homepage/sidebar/homepage-sidebar.controller.js
@@ -2,5 +2,22 @@ import { headerMenu } from '../fixture';
 
 /* @ngInject */
 export default ($window, $rootScope, $scope) => {
-  // $scope.menu = headerMenu;
+  $scope.menu = headerMenu;
+  $scope.catalogClicked = false;
+
+  /* On header menu click */
+  $scope._onSubMenuClick = (subMenu) => {
+    $scope.menu.map(item => {
+      if(item.title === subMenu) {
+        $scope.catalogClicked = !$scope.catalogClicked;
+        if($scope.catalogClicked) {
+          $scope.catalogActive = true;
+        } else {
+          $scope.catalogActive = false;
+        }
+        $scope.catalogName = subMenu.toLowerCase();
+        $scope.categories = item.categories;
+      }
+    })
+  }
 }
