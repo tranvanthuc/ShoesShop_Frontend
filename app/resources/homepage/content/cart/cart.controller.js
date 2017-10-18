d536f87491464f6d901445654145e343d9ebd5a3
Dung Le <dungle1811@gmail.com>
2017-10-18 15:28:41 +0700

    * Fix order action logic. + Implement remove order action.

--- app/resources/homepage/content/cart/cart.controller.js
+++ app/resources/homepage/content/cart/cart.controller.js
@@ -4,5 +4,18 @@ export default ($rootScope, $scope, $localStorage) => {
     $('#cart-container').css('padding-top', headerHeight + 'px'); 
   });
 
+  // $localStorage.$reset();
   $scope.orders = $localStorage.orders;
+
+  $scope._onClickRemove = () => {
+    $('#deleteOrderModal').modal();
+  }
+
+  $scope._removeOrder = order => {
+    const index = $scope.orders.indexOf(order);
+    if(index != -1) {
+      $scope.orders.splice(index, 1);
+    }
+    $('#deleteOrderModal').modal('hide');
+  }
 }
